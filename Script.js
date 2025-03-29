6ad11c57323dcfd89b5285b5c304554f08549e30
rohanroy1234 <royrohan12112008@gmail.com>
2025-03-29 20:10:38 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -7,21 +7,3 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
-.navbar {
-  background-color: #1a1a1a;
-  color: white;
-  padding: 10px;
-  display: flex;
-  align-items: center;
-}
-
-.logo {
-  height: 40px;
-  width: 40px;
-  margin-right: 10px;
-}
-
-.title {
-  font-size: 24px;
-  font-weight: bold;
-}
